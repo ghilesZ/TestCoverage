5a233be050474c573d25fdc08f316db72c2686fa
ghilesZ <ghiles.ziat@gmail.com>
2022-07-05 16:51:34 +0200

    add runtime to project

---
+++ coverage/runtime/cover_runtime.ml
@@ -0,0 +1,6 @@
+let counters : (Location.t, int) Hashtbl.t = Hashtbl.create 97
+
+let increment loc =
+  match Hashtbl.find_opt counters loc with
+  | None -> Hashtbl.add counters loc 1
+  | Some n -> Hashtbl.replace counters loc (n+1)
