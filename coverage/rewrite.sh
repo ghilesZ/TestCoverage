a6193c15ee69c263d08b0fb69fee03e09ef6115d
ghilesZ <ghiles.ziat@gmail.com>
2022-07-05 17:11:15 +0200

    compile script

---
+++ coverage/rewrite.sh
@@ -0,0 +1,14 @@
+set -euC
+
+if [ $# -eq 0 ]; then
+    echo 'Error: was expecting some OCaml file'
+    exit 1
+fi
+fbname=$(basename "$1" .ml)
+dir="testdir_$fbname"
+rm -rf "$dir"
+mkdir -p "$dir"
+dune exec src/rewriter.exe -- "$1" > "$dir/a.ml"
+echo "(executable (name a)(libraries cover_runtime)) " > "$dir/dune"
+dune build "$dir"
+cd "$dir"
